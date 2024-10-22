05e47687451b9dccd9940e318bbc906a571eecc6
kwakseobang <iii6602@naver.com>
2024-10-22 17:34:23 +0900

    질문 목록 리스트 작업

--- src/main/java/com/kms/SBB/controller/question/questionController.java
+++ src/main/java/com/kms/SBB/controller/question/questionController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -27,5 +28,13 @@ public class questionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
+    }
+    //요청한 URL인 http://localhost:8080/question/detail/2의 숫자 2처럼 변하는
+    // id값을 얻을 때에는 @PathVariable 애너테이션을 사용한다
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question",question);
+        return "question_detail";
     }
 }
--- src/main/java/com/kms/SBB/service/QuestionService.java
+++ src/main/java/com/kms/SBB/service/QuestionService.java
@@ -2,11 +2,13 @@ package com.kms.SBB.service;
 
 
 import com.kms.SBB.entitiy.question.Question;
+import com.kms.SBB.exception.DataNotFoundException;
 import com.kms.SBB.repository.question.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +18,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    //질문 데이터 조회
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question is Not Found..");
+        }
     }
 }
